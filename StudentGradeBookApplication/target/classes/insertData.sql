253160b13dbf8f7857d4250feadcbffad7c06dd7
Aashish Bansal <aashish22bansal@gmail.com>
2023-09-16 00:13:53 +0530

    StudentGradeBookApplication - Hidden Files
    StudentGradeBookApplication - Hidden Files

---
+++ StudentGradeBookApplication/target/classes/insertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO student(id, firstname, lastname, email_address) VALUES(12, 'StudentBfirstname', 'StudentBlastname', 'studentBemailaddress@gmail.com');
+INSERT INTO student(id, firstname, lastname, email_address) VALUES(13, 'StudentCfirstname', 'StudentClastname', 'studentCemailaddress@gmail.com');
+INSERT INTO student(id, firstname, lastname, email_address) VALUES(14, 'StudentDfirstname', 'StudentDlastname', 'studentDemailaddress@gmail.com');
+INSERT INTO student(id, firstname, lastname, email_address) VALUES(15, 'StudentEfirstname', 'StudentElastname', 'studentEemailaddress@gmail.com');
